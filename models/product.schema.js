1e4575b629f6c902709d51da5d1977dbc8102d94
athaur <110320341+athaurmd@users.noreply.github.com>
2022-12-13 17:40:29 +0000

    updated product schema

--- models/product.schema.js
+++ models/product.schema.js
@@ -9,6 +9,32 @@ const productSchema = new mongoose.Schema(
             trim : true,
             maxLength : [20, "Product name should not be greater than 20 characters"],
         },
+        
+        price : {
+            type : Number,
+            required : [true, "Please provide Price of product"],
+            maxLength : [6, "Price must not be greater than 6 digits"],
+        },
+        description : {
+            type : String,
+            //Assignment to add npm editor and markdown
+        },
+        photos :[ {
+            _url : {
+                type : String,
+                required : true
+            }
+            
+        }],
+        stock : {
+            type : Number,
+            default : 0
+        },
+        collectionId : {
+            type :mongoose.Schema.Types.ObjectId,
+            ref : "Collection"
+        }
+        
 
 },
 {
