ff27df78b97ee20d827c80654540dcaa6c4ddeb8
athaur <110320341+athaurmd@users.noreply.github.com>
2022-12-19 17:29:48 +0000

    added morgan and express middlewares

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors'
 
 const app = express();
+
+app.use(express.json);
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(cookieParser())
+
+app.use(morgan('tiny'))
 
 
 
